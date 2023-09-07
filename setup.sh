eaf091790273b587c9d9bb52f8d0cba0141414b8
Jeremy Yonathan <61494789+shigeru22@users.noreply.github.com>
2023-09-08 00:05:54 +0700

    project: Initial Bash configuration

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Linking Bash files..."
+
+if [[ -f "$HOME/.bash_profile" ]]; then
+	echo "  Backing up ~/.bash_profile..."
+	mv $HOME/.bash_profile $HOME/.bash_profile.bak
+fi
+
+if [[ -f "$HOME/.bashrc" ]]; then
+	echo "  Backing up ~/.bashrc..."
+	mv $HOME/.bashrc $HOME/.bashrc.bak
+fi
+
+echo "  Linking ~/.bash_profile"
+ln -s $PWD/bash/bash_profile $HOME/.bash_profile
+
+echo "  Linking ~/.bashrc..."
+ln -s $PWD/bash/bashrc $HOME/.bashrc
+
+# echo "  Linking ~/.blerc..."
+# ln -s $PWD/bash/blerc $HOME/.blerc
